048f42992c5ec0340c5f752c286f117a1f82c4b2
Chinmay Wyawahare <chinmaynw@gmail.com>
2018-11-18 01:44:06 -0500

    Added form validation

--- views/js/api.js
+++ views/js/api.js
@@ -1,85 +1,150 @@
-$(document).ready(function() {
-    
-    $("#register-button").on('click', function() {
-        var registerData={
-            "firstName": $("#inputFirstName").val(),
-            "lastName" : $("#inputLastName").val(),
-            "email": $("#inputRegisterEmail").val(),
-            "password":$("#inputRegisterPassword").val()
-        }
-        $.ajax({
-            url: "http://localhost:9090/register/",
-            method: "POST",
-            data: JSON.stringify(registerData),
-            dataType:'json',
-            contentType:"application/json",
-            success: function(data) {
-                //var msg = $('#inputFirstName').val();
-                toatr.success(data.firstName);
-            },
-        });
-    });
 
-});
+function registerUser() {
 
-function followUser(followerId, action, event){
-    var data={
-        "followerId" : parseInt(followerId),
+    var firstName = document.getElementById("inputFirstName").value;
+    var lastName = document.getElementById("inputLastName").value;
+    var email = document.getElementById("inputRegisterEmail").value;
+    var password = document.getElementById("inputRegisterPassword").value;
+
+    if (!firstName) {
+        toastr.error("Please enter First Name!")
+        return
     }
-    var url ="http://localhost:9090/"
-    url+= (action == "Follow")? "follow/":"unfollow/";
+    if(!lastName) {
+        toastr.error("Please enter Last Name!")
+        return
+    }
+    if(!email) {
+        toastr.error("Please enter Email!")
+        return
+    }
+    if(!password) {
+        toastr.error("Please enter Password!")
+        return
+    }
+
+    var registerData = {
+        "firstName": firstName,
+        "lastName": lastName,
+        "email": email,
+        "password": password
+    }
+    $.ajax({
+        url: "http://localhost:9090/register/",
+        method: "POST",
+        data: JSON.stringify(registerData),
+        dataType: 'json',
+        contentType: "application/json",
+        success: function (data) {
+            if (data.Status == 200) {
+                toastr.success(data.Message);
+            } else {
+                toastr.error(data.Message)
+            }
+
+        },
+    });
+}
+
+function followUser(followerId, action, event) {
+    var data = {
+        "followerId": parseInt(followerId),
+    }
+    var url = "http://localhost:9090/"
+    url += (action == "Follow") ? "follow/" : "unfollow/";
     $.ajax({
         url: url,
         method: "POST",
         data: JSON.stringify(data),
-        dataType:'json',
-        contentType:"application/json",
-        success: function(data) {
-            action = (action == "Follow")? "UnFollow":"Follow";
-            event.target.setAttribute("onclick","followUser('"+followerId+"','"+action+"',event)");    
+        dataType: 'json',
+        contentType: "application/json",
+        success: function (data) {
+            action = (action == "Follow") ? "UnFollow" : "Follow";
+            event.target.setAttribute("onclick", "followUser('" + followerId + "','" + action + "',event)");
             event.target.innerHTML = action
         },
     });
 }
 
-function signIn(){
-    var data={
-        "email": document.getElementById("inputLoginEmail").value,
-        "password" : document.getElementById("inputLoginPassword").value,
+function signIn() {
+    var email = document.getElementById("inputLoginEmail").value;
+    var password = document.getElementById("inputLoginPassword").value;
+
+    if(!email) {
+        toastr.error("Please enter Email!")
+        return
     }
-    var url ="http://localhost:9090/login/";
+    if(!password) {
+        toastr.error("Please enter Password!")
+        return
+    }
+
+    var data = {
+        "email": email,
+        "password": password,
+    }
+    var url = "http://localhost:9090/login/";
     $.ajax({
         url: url,
         method: "POST",
         data: JSON.stringify(data),
-        dataType:'json',
-        contentType:"application/json",
-        success: function(data) {
-            if(data.Status == 200){
-                location.href="http://localhost:9090/posts/";
+        dataType: 'json',
+        contentType: "application/json",
+        success: function (data) {
+            if (data.Status == 200) {
+                toastr.success(data.Message)
+                location.href = "http://localhost:9090/posts/";
+            }
+            else {
+                toastr.error(data.Message)
             }
         },
     });
 }
 
 
-function addPost(userId ,status){
-    var data={
-        "userId": userId,
-        "status" : status,
+function addPost() {
+    var status = document.getElementById("status").value
+
+    if (!status) {
+        toastr.error("Please enter status!!")
+        return
     }
-    var url ="http://localhost:9090/posts/"
+    var data = {
+        "status": status
+    }
+    var url = "http://localhost:9090/posts/"
 
     $.ajax({
         url: url,
         method: "POST",
         data: JSON.stringify(data),
-        dataType:'json',
-        contentType:"application/json",
-        success: function(data) {
-            //action = (action == "Follow")? "UnFollow":"Follow";
-            //event.target.setAttribute("onclick","followUser("+userId+",'"+followerId+"','"+action+"',event)");    
-            //event.target.innerHTML = action
+        dataType: 'json',
+        contentType: "application/json",
+        success: function (data) {
+            if (data.Status == 200) {
+                toastr.success(data.Message);
+            } else {
+                toastr.error(data.Message)
+            }
+        },
+    });
+}
+
+function logout() {
+    var url = "http://localhost:9090/logout/"
+    $.ajax({
+        url: url,
+        method: "DELETE",
+        dataType: 'json',
+        contentType: "application/json",
+        success: function (data) {
+            if (data.Status == 200) {
+                toastr.success(data.Message);
+                location.href = "http://localhost:9090/login/";
+            } else {
+                toastr.error(data.Message)
+            }
         },
     });
 }
