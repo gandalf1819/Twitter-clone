7ae61e0cb1162608f96b62b0058ea277576b495d
Nikhil Nar <ncn251@nyu.edu>
2018-11-30 01:26:31 -0500

    Created Auth Service

---
+++ services/auth/auth_driver/driver.go
@@ -0,0 +1,79 @@
+package auth_driver
+
+import (
+	"../authpb"
+	"context"
+	"errors"
+	"log"
+	"math/rand"
+)
+
+type Server struct{}
+
+type TokenDB struct {
+	t *authpb.AuthToken
+}
+
+var db TokenDB
+
+func Init() {
+	tok := NewToken()
+	db.t = tok
+	log.Println("DB Token Initialized =", db.t.Token)
+}
+
+func NewToken() *authpb.AuthToken {
+	return &authpb.AuthToken{
+		Token: make(map[string]int32),
+	}
+}
+
+func (*Server) GetUserIdFromToken(ctx context.Context, tokName *authpb.AuthTokenName) (*authpb.AuthTokenValue, error) {
+	token := &authpb.AuthTokenValue{}
+	userId, ok := db.t.Token[tokName.TokenName]
+	if ok {
+		token.TokenValue = userId
+		return token, nil
+	} else {
+		return token, errors.New("No token found or token expired!!")
+	}
+}
+
+func (s *Server) AddToken(ctx context.Context, userId *authpb.UserId) (*authpb.AuthTokenName, error) {
+	tok, err := s.GenerateToken(ctx, &authpb.InitToken{})
+
+	if err != nil {
+		log.Println("Error occured while adding token for userId", userId.User, " = ", err)
+		return &authpb.AuthTokenName{TokenName: ""}, err
+	}
+
+	db.t.Token[tok.TokenName] = userId.User
+
+	log.Println("Token generated for userId ", userId.User, " = ", tok.TokenName)
+	log.Println("TokenDB = ", db.t.Token)
+
+	return tok, err
+}
+
+func (*Server) UnsetToken(ctx context.Context, tokName *authpb.AuthTokenName) (*authpb.Status, error) {
+	status := &authpb.Status{}
+	delete(db.t.Token, tokName.TokenName)
+	log.Println("Token ", tokName.TokenName, " deleted successfully")
+	log.Println("TokenDB = ", db.t.Token)
+	return status, nil
+}
+
+func (*Server) GenerateToken(ctx context.Context, in *authpb.InitToken) (*authpb.AuthTokenName, error) {
+	letterBytes := "abcdefghijklmnopqrstuvwxyz"
+	b := make([]byte, 16)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	status := &authpb.AuthTokenName{
+		TokenName: string(b),
+	}
+
+	log.Println("Token Generated = ", status.TokenName)
+
+	return status, nil
+}
--- views/js/api.js
+++ views/js/api.js
@@ -1,149 +1,149 @@
 
 function registerUser() {
 
-    var firstName = document.getElementById("inputFirstName").value;
-    var lastName = document.getElementById("inputLastName").value;
-    var email = document.getElementById("inputRegisterEmail").value;
-    var password = document.getElementById("inputRegisterPassword").value;
+  var firstName = document.getElementById("inputFirstName").value;
+  var lastName = document.getElementById("inputLastName").value;
+  var email = document.getElementById("inputRegisterEmail").value;
+  var password = document.getElementById("inputRegisterPassword").value;
 
-    if (!firstName) {
-        toastr.error("Please enter First Name!")
-        return
-    }
-    if (!lastName) {
-        toastr.error("Please enter Last Name!")
-        return
-    }
-    if (!email) {
-        toastr.error("Please enter Email!")
-        return
-    }
-    if (!password) {
-        toastr.error("Please enter Password!")
-        return
-    }
+  if (!firstName) {
+      toastr.error("Please enter First Name!")
+      return
+  }
+  if (!lastName) {
+      toastr.error("Please enter Last Name!")
+      return
+  }
+  if (!email) {
+      toastr.error("Please enter Email!")
+      return
+  }
+  if (!password) {
+      toastr.error("Please enter Password!")
+      return
+  }
 
-    var registerData = {
-        "firstName": firstName,
-        "lastName": lastName,
-        "email": email,
-        "password": password
-    }
-    $.ajax({
-        url: "http://localhost:9090/register/",
-        method: "POST",
-        data: JSON.stringify(registerData),
-        dataType: 'json',
-        contentType: "application/json",
-        success: function (data) {
-            if (data.Status == 200) {
-                toastr.success(data.Message);
-            } else {
-                toastr.error(data.Message)
-            }
+  var registerData = {
+      "firstName": firstName,
+      "lastName": lastName,
+      "email": email,
+      "password": password
+  }
+  $.ajax({
+      url: "http://localhost:9090/register/",
+      method: "POST",
+      data: JSON.stringify(registerData),
+      dataType: 'json',
+      contentType: "application/json",
+      success: function (data) {
+          if (data.Status == 200) {
+              toastr.success(data.Message);
+          } else {
+              toastr.error(data.Message)
+          }
 
-        },
-    });
+      },
+  });
 }
 
 function followUser(followerId, action, event) {
-    var data = {
-        "followerId": parseInt(followerId),
-    }
-    var url = "http://localhost:9090/"
-    url += (action == "Follow") ? "follow/" : "unfollow/";
-    $.ajax({
-        url: url,
-        method: "POST",
-        data: JSON.stringify(data),
-        dataType: 'json',
-        contentType: "application/json",
-        success: function (data) {
-            action = (action == "Follow") ? "UnFollow" : "Follow";
-            event.target.setAttribute("onclick", "followUser('" + followerId + "','" + action + "',event)");
-            event.target.innerHTML = action
-        },
-    });
+  var data = {
+      "followerId": parseInt(followerId),
+  }
+  var url = "http://localhost:9090/"
+  url += (action == "Follow") ? "follow/" : "unfollow/";
+  $.ajax({
+      url: url,
+      method: "POST",
+      data: JSON.stringify(data),
+      dataType: 'json',
+      contentType: "application/json",
+      success: function (data) {
+          action = (action == "Follow") ? "UnFollow" : "Follow";
+          event.target.setAttribute("onclick", "followUser('" + followerId + "','" + action + "',event)");
+          event.target.innerHTML = action
+      },
+  });
 }
 
 function signIn() {
-    var email = document.getElementById("inputLoginEmail").value;
-    var password = document.getElementById("inputLoginPassword").value;
+  var email = document.getElementById("inputLoginEmail").value;
+  var password = document.getElementById("inputLoginPassword").value;
 
-    if (!email) {
-        toastr.error("Please enter Email!")
-        return
-    }
-    if (!password) {
-        toastr.error("Please enter Password!")
-        return
-    }
+  if (!email) {
+      toastr.error("Please enter Email!")
+      return
+  }
+  if (!password) {
+      toastr.error("Please enter Password!")
+      return
+  }
 
-    var data = {
-        "email": email,
-        "password": password,
-    }
-    var url = "http://localhost:9090/login/";
-    $.ajax({
-        url: url,
-        method: "POST",
-        data: JSON.stringify(data),
-        dataType: 'json',
-        contentType: "application/json",
-        success: function (data) {
-            if (data.Status == 200) {
-                toastr.success(data.Message)
-                location.href = "http://localhost:9090/posts/";
-            }
-            else {
-                toastr.error(data.Message)
-            }
-        },
-    });
+  var data = {
+      "email": email,
+      "password": password,
+  }
+  var url = "http://localhost:9090/login/";
+  $.ajax({
+      url: url,
+      method: "POST",
+      data: JSON.stringify(data),
+      dataType: 'json',
+      contentType: "application/json",
+      success: function (data) {
+          if (data.Status == 200) {
+              toastr.success(data.Message)
+              location.href = "http://localhost:9090/posts/";
+          }
+          else {
+              toastr.error(data.Message)
+          }
+      },
+  });
 }
 
 
 function addPost() {
-    var status = document.getElementById("status").value
-    if (!status) {
-        toastr.error("Please enter status!!")
-        return
-    }
-    var data = {
-        "status": status
-    }
-    var url = "http://localhost:9090/posts/"
+  var status = document.getElementById("status").value
+  if (!status) {
+      toastr.error("Please enter status!!")
+      return
+  }
+  var data = {
+      "status": status
+  }
+  var url = "http://localhost:9090/posts/"
 
-    $.ajax({
-        url: url,
-        method: "POST",
-        data: JSON.stringify(data),
-        dataType: 'json',
-        contentType: "application/json",
-        success: function (data) {
-            if (data.Status == 200) {
-                toastr.success(data.Message);
-            } else {
-                toastr.error(data.Message)
-            }
-        },
-    });
+  $.ajax({
+      url: url,
+      method: "POST",
+      data: JSON.stringify(data),
+      dataType: 'json',
+      contentType: "application/json",
+      success: function (data) {
+          if (data.Status == 200) {
+              toastr.success(data.Message);
+          } else {
+              toastr.error(data.Message)
+          }
+      },
+  });
 }
 
 function logout() {
-    var url = "http://localhost:9090/logout/"
-    $.ajax({
-        url: url,
-        method: "DELETE",
-        dataType: 'json',
-        contentType: "application/json",
-        success: function (data) {
-            if (data.Status == 200) {
-                toastr.success(data.Message);
-                location.href = "http://localhost:9090/login/";
-            } else {
-                toastr.error(data.Message)
-            }
-        },
-    });
+  var url = "http://localhost:9090/logout/"
+  $.ajax({
+      url: url,
+      method: "DELETE",
+      dataType: 'json',
+      contentType: "application/json",
+      success: function (data) {
+          if (data.Status == 200) {
+              toastr.success(data.Message);
+              location.href = "http://localhost:9090/login/";
+          } else {
+              toastr.error(data.Message)
+          }
+      },
+  });
 }
